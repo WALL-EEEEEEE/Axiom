9782d3e66abcc2ad94ee0cbf8d7a901863627c10
Wall'e <duanqingbb@163.com>
2023-09-03 17:28:31 +0800

    add a test utility for table-driven tests

--- core/stream/stream_test.go
+++ core/stream/stream_test.go
@@ -2,21 +2,68 @@ package stream
 
 import (
 	"testing"
+
+	. "github.com/WALL-EEEEEEE/Axiom/test"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestStreamPass(t *testing.T) {
-	broker := NewBroker[int]()
-	istream := broker.GetInputStream()
-	ostream := broker.GetOutputStream()
-	cnt := 10
-	go func() {
-		for i := 0; i < cnt; i++ {
-			istream.In() <- i
-			t.Logf("Input: %d", i)
-		}
-
-	}()
-	for i := 0; i < cnt; i++ {
-		t.Logf("Output: %d", <-ostream.Out())
+func TestStream(t *testing.T) {
+	cases := []TestCase[any, any]{
+		{
+			Name:     "GetName",
+			Input:    "Test",
+			Error:    nil,
+			Expected: "Test",
+			Func: func(tc TestCase[any, any]) {
+				stream := NewStream[int](tc.Input.(string))
+				print(stream.GetName())
+				assert.Equal(t, tc.Expected, stream.GetName())
+			},
+		},
+		{
+			Name:     "InOut",
+			Input:    []int{1, 2, 3, 4},
+			Error:    nil,
+			Expected: []int{1, 2, 3, 4},
+			Func: func(tc TestCase[any, any]) {
+				stream := NewStream[int](tc.Name)
+				var expected []int
+				go func() {
+					for _, item := range tc.Input.([]int) {
+						//t.Logf("stream %s <- %+v", stream.GetName(), item)
+						stream.In() <- item
+					}
+					close(stream.In())
+				}()
+				for item := range stream.Out() {
+					//t.Logf("stream %s -> %+v", stream.GetName(), item)
+					expected = append(expected, item)
+				}
+				assert.Equal(t, tc.Expected, expected)
+			},
+		},
+		{
+			Name:     "Via",
+			Input:    []int{1, 2, 3, 4},
+			Error:    nil,
+			Expected: []int{1, 2, 3, 4},
+			Func: func(tc TestCase[any, any]) {
+				stream := NewStream[int](tc.Name)
+				var expected []int
+				go func() {
+					for _, item := range tc.Input.([]int) {
+						//t.Logf("stream %s <- %+v", stream.GetName(), item)
+						stream.In() <- item
+					}
+					close(stream.In())
+				}()
+				for item := range stream.Out() {
+					//t.Logf("stream %s -> %+v", stream.GetName(), item)
+					expected = append(expected, item)
+				}
+				assert.Equal(t, tc.Expected, expected)
+			},
+		},
 	}
+	Run(cases, t)
 }
